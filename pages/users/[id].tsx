745f1214b461ccfd0ca08958ecfb4f0a47405d36
Mink-a <rahulakhant@gmail.com>
2022-09-22 16:38:09 +0630

    getStaticPaths

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,9 +1,35 @@
 import { NextPage } from "next";
+import { Key } from "react";
 
-interface Props {}
+export const getStaticPaths = async () => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await response.json();
 
-const DynamicePage: NextPage<Props> = ({}) => {
-  return <div>Helo This is dynamic page.</div>;
+  const paths = data.map((user: { id: String }) => {
+    return { params: { id: user.id.toString() } };
+  });
+
+  return { paths: paths, fallback: false };
+};
+
+export const getStaticProps = async (context: { params: { id: String } }) => {
+  const id = context.params.id;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  const data = await response.json();
+
+  return {
+    props: { user: data },
+  };
+};
+
+interface Props {
+  user: { name: String };
+}
+
+const DynamicePage: NextPage<Props> = ({ user }) => {
+  return <div>{user.name}</div>;
 };
 
 export default DynamicePage;
