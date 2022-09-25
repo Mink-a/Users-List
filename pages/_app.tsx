afb17196c1442a664403e1d7505a134ab396d636
Mink-a <rahulakhant@gmail.com>
2022-09-25 22:24:45 +0630

    added page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,40 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={router.route}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 0.75,
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+            clipPath: "polygon(100% 0, 0 0, 0 100%, 100% 100%)",
+          },
+          animateState: {
+            opacity: 1,
+            clipPath: "polygon(100% 0, 0 0, 0 100%, 100% 100%)",
+          },
+          exitState: {
+            clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+          },
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
